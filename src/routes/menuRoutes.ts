4f55ebb49fd691c3448b06497a8d9b0773742f8d
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-08-26 14:08:30 -0600

    feat: add protected owner menu listing

--- src/routes/menuRoutes.ts
+++ src/routes/menuRoutes.ts
@@ -4,17 +4,28 @@ import { authenticate } from "../middlewares/authMiddleware";
 import { authorize } from "../middlewares/roleMiddleware";
 import {
   requireMenuOwnership,
+  requireBusinessOwnership,
   requireBusinessOwnershipFromBody,
 } from "../middlewares/ownership";
-import { createMenuValidation } from "../validators/menuValidators";
+import {
+  createMenuValidation,
+  updateMenuValidation,
+} from "../validators/menuValidators";
 import { validateRequest } from "../middlewares/validationMiddleware";
 
 const router = Router();
 const menuController = new MenuController();
 
 router.get("/", menuController.getAll);
-router.get("/:id", menuController.getById);
 router.get("/business/:businessId", menuController.getByBusiness);
+router.get(
+  "/business/:businessId/manage",
+  authenticate,
+  authorize("admin", "owner"),
+  requireBusinessOwnership("businessId"),
+  menuController.getManagedByBusiness,
+);
+router.get("/:id", menuController.getById);
 
 router.post(
   "/",
@@ -22,7 +33,7 @@ router.post(
   authorize("admin", "owner"),
   createMenuValidation,
   validateRequest,
-  requireBusinessOwnershipFromBody("business_id"), // dueño del negocio del body
+  requireBusinessOwnershipFromBody("business_id"),
   menuController.create,
 );
 
@@ -30,7 +41,9 @@ router.put(
   "/:id",
   authenticate,
   authorize("admin", "owner"),
-  requireMenuOwnership("id"), // dueño del negocio de ESE producto
+  requireMenuOwnership("id"),
+  updateMenuValidation,
+  validateRequest,
   menuController.update,
 );
 
