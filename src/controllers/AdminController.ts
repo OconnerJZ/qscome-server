7ddfe2b354b3fca7fbd7902553dbf63a7e1023be
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-09-06 14:57:59 -0600

    feat: expose plan impact preview controller

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { AdminService } from "../services/AdminService";
+import { BusinessPlanImpactService } from "../services/BusinessPlanImpactService";
 
 export class AdminController {
   private readonly service = new AdminService();
+  private readonly planImpact = new BusinessPlanImpactService();
 
   searchBusinesses = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -10,6 +12,20 @@ export class AdminController {
       res.json({
         success: true,
         data: await this.service.searchBusinesses(String(req.query.q || ""), limit),
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  previewPlanImpact = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.json({
+        success: true,
+        data: await this.planImpact.preview(
+          Number(req.params.id),
+          String(req.query.planCode || ""),
+        ),
       });
     } catch (error) {
       next(error);
